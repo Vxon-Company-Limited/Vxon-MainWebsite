2daea3981bcd47370e0c155582c15ffa70f98924
Ch1rpy <ch1rpy@163.com>
2026-04-16 11:07:32 +0800

    feat: add source utility

---
+++ src/lib/source.ts
@@ -0,0 +1,17 @@
+import { docs } from '@/.source';
+import { type InferPageType, loader } from 'fumadocs-core/source';
+import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { openapiPlugin } from 'fumadocs-openapi/server';
+import { i18n } from '@/lib/i18n';
+
+export const source = loader({
+  baseUrl: '/docs',
+  i18n,
+  source: docs.toFumadocsSource(),
+  plugins: [
+    lucideIconsPlugin(),
+    openapiPlugin(),
+  ],
+});
+
+export type DocsPageType = InferPageType<typeof source>;
